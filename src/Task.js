ce16a78bf06e0354bbba0fe6414065feb0ed1dcd
Rafli Fahreza <raflifahreza11@gmail.com>
2022-12-15 19:51:48 +0700

    first upload file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Rafli Fahreza" // TODO: replace this
+const studentId = "FE2789195" // TODO: replace this
+
+export { url, studentName, studentId }
